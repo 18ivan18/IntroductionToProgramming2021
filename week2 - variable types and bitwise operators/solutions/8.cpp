39cd56a3b2c7d1e8e7eb3aa41d2d0ed7bec0ecbd
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2021-10-11 23:11:35 +0300

    add week2 solutions

---
+++ week2 - variable types and bitwise operators/solutions/8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double number;
+
+    std::cin >> number;
+
+    int integerPart = floor(number);
+    //	еквивалентно:
+    //	int integerPart = (int)number;
+    //	int integerPart = int(number);
+    //	int integerPart = static_cast<int>(number);
+
+    std::cout << integerPart;
+    return 0;
+}
